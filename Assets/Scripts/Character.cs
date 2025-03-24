3288b633f6f62e8c4f9924f9870086cf8af3319b
CNyaha <llhb0625@gmail.com>
2025-03-25 08:54:53 +0900

    11

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerClass
+{
+    내배캠수강생,
+
+}
+
+public class Character : MonoBehaviour
+{
+    
+    public string playerName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
